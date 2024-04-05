e08fa5204d07dbe8998f40b41c478f339c5f40d2
Shrey-Singhal <s3916734@student.rmit.edu.au>
2024-04-06 10:54:37 +1100

    basic register page

---
+++ frontend/src/pages/Register.js
@@ -0,0 +1,25 @@
+import {Form} from "react-router-dom";
+import {useState} from "react";
+const Register = () => {
+    const [username, setUsername] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [errorMessage, setErrorMessage] = useState("");
+
+
+    return (
+        <div>
+            <h1>Register</h1>
+            <form>
+                <label>Username:</label>
+                <input type = "username" value={username} onChange ={(e) => setUsername(e.target.value)} required />
+                <label>Email:</label>
+                <input type ="email" value={email} onChange={(e) => setEmail(e.target.value)} required/>
+                <label>Password:</label>
+                <input type = "password" value = {password} onChange={(e) => setPassword(e.target.value)} required/>
+            </form>
+            {errorMessage && <p>{errorMessage}</p>}
+        </div>
+    )
+}
+export default Register;
