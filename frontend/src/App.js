c7a6fc6f78542d963d5e63b8e4355a36b8dee120
Shrey-Singhal <s3916734@student.rmit.edu.au>
2024-04-12 00:09:36 +1000

    added navbar and tailwind css

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,9 +3,11 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Login from "./pages/Login";
 import MainPage from "./pages/MainPage";
 import Register from "./pages/Register";
+import Navbar from "./Component/Navbar";
 function App() {
   return (
       <Router>
+          <Navbar />
           <Routes>
               <Route path="/" element={<Login />} />
               <Route path="/mainpage" element={<MainPage />} />
---
+++ frontend/src/Component/Navbar.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Link } from 'react-router-dom'; // Assuming you're using react-router for navigation
+
+const Navbar = () => {
+    return (
+        <nav className="bg-gray-800 text-white p-4">
+            <div className="container px-14 flex justify-between items-center">
+                <div className="text-lg font-semibold">
+                    <Link to="/mainpage" className="hover:text-gray-300">Music Subscription App</Link>
+                </div>
+                <div className="md:flex space-x-4">
+                    <Link to="/">
+                        <button className="hover:text-gray-300">Login</button>
+                    </Link>
+                    <Link to="/mainpage">
+                        <button className="hover:text-gray-300">Query Area</button>
+                    </Link>
+                </div>
+            </div>
+        </nav>
+    );
+};
+
+export default Navbar;
