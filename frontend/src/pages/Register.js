619b347db99fdcffa3ff41d04e4c4925effb7ad5
Shrey-Singhal <s3916734@student.rmit.edu.au>
2024-04-12 02:09:29 +1000

    Register page styling

--- frontend/src/pages/Register.js
+++ frontend/src/pages/Register.js
@@ -39,21 +39,32 @@ const Register = () => {
     };
 
     return (
-        <div>
-            <h1>Register</h1>
-            <form onSubmit={handleSubmit}>
-                <label>Username:</label>
-                <input type = "username" value={username} onChange ={(e) => setUsername(e.target.value)} required />
-                <br />
-                <label>Email:</label>
-                <input type ="email" value={email} onChange={(e) => setEmail(e.target.value)} required/>
-                <br />
-                <label>Password:</label>
-                <input type = "password" value = {password} onChange={(e) => setPassword(e.target.value)} required/>
-                <br/>
-                <button type="submit">Submit</button>
-            </form>
-            {errorMessage && <p>{errorMessage}</p>}
+        <div className="flex flex-col mt-20 items-center">
+            <h1 className="text-3xl font-bold mb-4">Register</h1>
+            <div className="w-[380px] p-4 border border-white border-opacity-30 rounded-md mt-inner_padding">
+                <form onSubmit={handleSubmit}>
+                    <div className="flex flex-col">
+                        <label>Username:</label>
+                        <input className="text-black rounded outline-none p-[5px] pl-[12px] pr-[12px]"
+                            type = "username" value={username} onChange ={(e) => setUsername(e.target.value)} required />
+                        <br />
+                        <label>Email:</label>
+                        <input className="text-black rounded outline-none p-[5px] pl-[12px] pr-[12px]"
+                            type ="email" value={email} onChange={(e) => setEmail(e.target.value)} required/>
+                        <br />
+                        <label>Password:</label>
+                        <input className="text-black rounded outline-none p-[5px] pl-[12px] pr-[12px]"
+                            type = "password" value = {password} onChange={(e) => setPassword(e.target.value)} required/>
+                        <br/>
+                    </div>
+                    <button type="submit"
+                            className="p-[5px] pl-[16px] pr-[16px] rounded-[6px] leading-5 cursor-pointer
+                    text-black bg-custom-color2 hover:brightness-90 w-full mb-1 mt-2"
+                    >Submit</button>
+                </form>
+                {errorMessage && <p>{errorMessage}</p>}
+            </div>
+
         </div>
     )
 }
