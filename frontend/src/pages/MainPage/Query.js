c11174f7a86fd04e99a9c8156fa89b16a623c2d2
Shrey-Singhal <s3916734@student.rmit.edu.au>
2024-04-12 23:20:25 +1000

    Query functionality

--- frontend/src/pages/MainPage/Query.js
+++ frontend/src/pages/MainPage/Query.js
@@ -1,15 +1,41 @@
 import React, {useState, useEffect} from "react";
+import axios from "axios";
 const Query = () => {
     const [artist, setArtist] = useState("");
     const [title, setTitle] = useState("");
     const [year, setYear] = useState("");
+    const [musicData, setMusicData] = useState([]);
+    const [isVisible, setIsVisible] = useState(false);
+
+
+    const handleSubmitQuery = async (event) => {
+        event.preventDefault();
+        const apiEndpoint = "https://sjj7ct221l.execute-api.us-east-1.amazonaws.com/Production/music_table_lambda_function";
+        axios.post(apiEndpoint,{
+            operation:'query',
+            artist:artist,
+            title:title,
+            year:year
+
+        }).then(response => {
+            console.log(response)
+            if (response.data.statusCode === 200) {
+                const responseBody = JSON.parse(response.data.body);
+                setMusicData(responseBody);
+                setIsVisible(true);
+            }
+            //console.log(responseBody);
+        })
+
+    };
+
 
     return (
         <div className="flex flex-col mt-14 px-20">
             <h1 className="flex text-3xl font-bold mb-2">Query Area</h1>
             <p>You can enter some information in any (or all) of these text areas and click the “Submit Query” button</p>
             <div className="w-[380px] p-4  rounded-md mt-2">
-                <form>
+                <form onSubmit={handleSubmitQuery}>
                     <div className="flex space-x-4">
                         <div>
                             <label className="font-bold">Artist:</label>
@@ -39,6 +65,38 @@ const Query = () => {
                 </form>
 
             </div>
+            {isVisible && (
+                <div className="mt-4">
+                    <table className="min-w-full divide-y">
+                        <thead>
+                        <tr>
+                            <th>Artist</th>
+                            <th>Title</th>
+                            <th>Year</th>
+                            <th>Image</th>
+                        </tr>
+                        </thead>
+                        <tbody className="bg-custom-color2 text-black divide-y divide-gray-200">
+                        {musicData.map((item, index) => (
+                            <tr key={index}>
+                                <td>{item.artist}</td>
+                                <td>{item.title}</td>
+                                <td>{item.year}</td>
+                                <td>
+                                    <img
+                                        src={item.img_url}
+                                        alt={`Cover for ${item.title}`}
+                                        className="w-20 h-20 object-cover"
+                                    />
+                                </td>
+                            </tr>
+                        ))}
+                        </tbody>
+                    </table>
+                </div>
+
+            )}
+
         </div>
     );
 
